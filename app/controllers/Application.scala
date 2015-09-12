4ce81196322b2f9a4269f8b483b4ded31eda9d35
pfwang <pfwang@uwaterloo.ca>
2015-09-11 23:34:46 -0400

    added db

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,16 +1,32 @@
 package controllers
 
 import play.api._
+import play.api.i18n.{Messages, I18nSupport}
 import play.api.mvc._
 import play.api.cache.Cache
 import play.api.Play.current
-
+import play.api.data._
+import play.api.data.Forms._
 import play.api.db._
+import play.api.i18n.I18nSupport
+import play.i18n.MessagesApi
+import play.api.Play.current
+import play.api.i18n.Messages.Implicits._
 
-object Application extends Controller {
+object Application extends Controller{
 
   def index = Action {
-    Ok(views.html.homepage())
+    val signUpForm = Form(
+      tuple(
+        "username" -> text,
+        "password" -> text
+      )
+    )
+    Ok(views.html.homepage(signUpForm))
+  }
+
+  def submit = Action {
+    Ok("good")
   }
 
   def db = Action {
